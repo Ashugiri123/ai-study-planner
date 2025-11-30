1062ecef2431787974535b11262e833c60107b4e
Ashugiri123 <ashugoswami339@gmail.com>
2025-11-30 22:28:56 +0530

    Implement LongTermMemory class for user profiles

--- memory/long_term_memory.py
+++ memory/long_term_memory.py
@@ -1,1 +1,27 @@
+import json
+import os
+
+class LongTermMemory:
+    def __init__(self, filename="user_profiles.json"):
+        self.filename = filename
+        if not os.path.exists(self.filename):
+            with open(self.filename, "w") as f:
+                json.dump({}, f)
+
+    def load_all(self):
+        with open(self.filename, "r") as f:
+            return json.load(f)
+
+    def save_all(self, data):
+        with open(self.filename, "w") as f:
+            json.dump(data, f, indent=2)
+
+    def load_profile(self, name):
+        data = self.load_all()
+        return data.get(name)
+
+    def save_profile(self, name, profile):
+        data = self.load_all()
+        data[name] = profile
+        self.save_all(data)
 
