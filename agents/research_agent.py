94d1f4f758bc02c4a06c09316badc38ebe46efa9
Ashugiri123 <ashugoswami339@gmail.com>
2025-11-30 22:26:17 +0530

    Implement ResearchAgent with resource search functionality
    Added ResearchAgent class to utilize SearchTool for finding resources.

--- agents/research_agent.py
+++ agents/research_agent.py
@@ -1,1 +1,11 @@
+from tools.search_tool import SearchTool
 
+class ResearchAgent:
+    def __init__(self):
+        self.search_tool = SearchTool()
+
+    def find_resources(self, subjects):
+        resources = {}
+        for subject in subjects:
+            resources[subject] = self.search_tool.search_resources(subject)
+        return resources
